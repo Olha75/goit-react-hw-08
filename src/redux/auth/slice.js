b38fd96e07cc26c13239a7ae2ac509cda0c0fb79
Olha75 <137830839+Olha75@users.noreply.github.com>
2024-04-13 16:30:36 +0300

    struct_aad_componens

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,9 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const authSlice=createSlice({
-    name:"auth",
-    initialState:{
-        user:{
+// const authSlice=createSlice({
+//     name:"auth",
+//     initialState:{
+//         user:{
+//       name: null,
+//       email: null,
+//     },
+//     token: null,
+//     isLoggedIn: false,
+//     isRefreshing: false,
+//   },
+// });
+
+// export default authSlice.reduser
+
+import { createSlice } from "@reduxjs/toolkit";
+import { logIn, logOut, refreshUser, register } from "./operations";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: {
       name: null,
       email: null,
     },
@@ -11,6 +29,34 @@ const authSlice=createSlice({
     isLoggedIn: false,
     isRefreshing: false,
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logOut.fulfilled, (state) => {
+        state.user = {
+          name: null,
+          email: null,
+        };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+      .addCase(refreshUser.pending, (state) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      }),
 });
 
-export default authSlice.reduser
+export default authSlice.reducer;
